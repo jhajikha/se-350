4a2db60bd375944f025986c447b576a29561c40f
jhajikha <julien.hajikhani@gmail.com>
2021-10-17 22:54:05 -0700

    first commit for part 2

---
+++ main/java/model/interfaces/Shape.java
@@ -0,0 +1,10 @@
+package model.interfaces;
+
+import java.awt.Graphics2D;
+
+/**
+ * Represents an individual shape drawn by the user.
+ */
+public interface Shape {
+  void draw(Graphics2D graphics);
+}
